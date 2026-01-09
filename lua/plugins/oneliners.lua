daf8dba28452d962d8729b04f8b719b627a505eb
Jägermeist <philipthorell@outlook.com>
2026-01-09 22:47:36 +0100

    Fixed spelling mistake on git-config.lua file name + removed vim-fugitive from the oneliners.lua file since its included in git-config.lua

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -2,9 +2,6 @@ return {
 	{ -- This helps with ssh tunneling and copying to clipboard
 		"ojroques/vim-oscyank",
 	},
-	{ -- Git plugin
-		"tpope/vim-fugitive",
-	},
 	{ -- Show CSS Colors
 		"brenoprata10/nvim-highlight-colors",
 		config = function()
