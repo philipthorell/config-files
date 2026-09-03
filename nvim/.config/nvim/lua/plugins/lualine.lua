56bdbcb58c0084ad269bf858b079165f9a4f1ed4
Jägermeist <philipthorell@outlook.com>
2026-09-03 23:53:06 +0200

    Added ipynb and configured lualine in nvim config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,12 +6,28 @@ return {
 				theme = "auto",
 			},
 			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = {
+					{
+						"filetype",
+						icon_only = true,
+						separator = "",
+						padding = { left = 1, right = 0 },
+					},
+					{
+						"filename",
+						padding = { left = 0, right = 1 },
+					},
+				},
+				lualine_x = { "encoding", "fileformat" },
 				lualine_y = {
 					{
 						"diagnostics",
 						sources = { "nvim_workspace_diagnostic" },
 					},
 				},
+				lualine_z = { "location" },
 			},
 		})
 	end,
